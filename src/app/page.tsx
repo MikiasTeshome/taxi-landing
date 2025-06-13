77a35d3ef7fe03e1da582f0bce4ae4067c1cccc2
Mikias Teshome <49233261+MikiasTeshome@users.noreply.github.com>
2025-06-13 21:46:19 +0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 import AboutUs from '@/components/AboutUs';
@@ -12,49 +14,60 @@ import SectionHeader from '@/components/SectionHeader';
 import SectionPricing from '@/components/SectionPricing';
 import SectionReviews from '@/components/SectionReviews';
 
-const page = () => {
+const HomePage = () => {
   return (
     <div className="relative">
+      {/* Hero/Header Section */}
       <div className="rounded-2xl bg-custom-gray py-20">
         <SectionHeader />
       </div>
 
+      {/* Download App Section */}
       <div className="rounded-b-2xl bg-custom-black pb-16 pt-24">
         <SectionDownloadApp />
       </div>
 
+      {/* Benefits Section */}
       <div className="py-20">
         <SectionBenefits />
       </div>
 
+      {/* About Us Section */}
       <div className="rounded-2xl bg-custom-gray py-24">
         <AboutUs />
       </div>
 
+      {/* Demo Section */}
       <div className="py-24">
         <SectionDemo />
       </div>
 
+      {/* Features Section */}
       <div className="rounded-2xl bg-custom-black pb-1 pt-24 md:h-[45vh] md:pb-0 lg:h-[85vh]">
         <SectionFeatures />
       </div>
 
+      {/* Pricing Section */}
       <div className="pb-24 pt-32 md:mt-32 lg:mt-72">
         <SectionPricing />
       </div>
 
+      {/* Location Slider */}
       <div className="relative mb-28 mt-7 bg-custom-black">
         <LocationSlider />
       </div>
 
+      {/* Reviews Section */}
       <div className="container mb-28">
         <SectionReviews />
       </div>
 
+      {/* Drivers Section */}
       <div className="rounded-2xl bg-custom-gray py-24">
         <SectionDrivers />
       </div>
 
+      {/* Blogs Section */}
       <div className="py-24">
         <SectionBlogs />
       </div>
@@ -62,4 +75,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default HomePage;
